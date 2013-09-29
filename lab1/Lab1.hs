1daec5be2d2b6c1c32dcfb3f0a9ffc541afcde92
Andrey Komarov <taukus@gmail.com>
2013-09-29 20:33:39 +0400

    pretty printing

--- lab1/Lab1.hs
+++ lab1/Lab1.hs
@@ -9,17 +9,44 @@ import System.Time
 import Data.Binary
 import Data.Binary.Put
 import Data.Binary.Get
+import Data.Monoid
 import Data.Word
+import Data.Function
+import Data.List
+
+import Text.Printf
 
 import Control.Concurrent
 import Control.Applicative
 
-port = 44444
+import qualified Data.Map as M
+
+port = 1234
 sendTimeout = 1000000
 killTimeout = 15000000
+printTimeout = 5000000
 
 hostnameLength = 20
 usernameLength = 20
+
+data Messages = Messages
+    { messagesCounts :: M.Map Msg Int
+    , messagesKillerHandler :: M.Map Msg ThreadId
+    }
+
+instance Show Messages where
+    show (Messages c _) = let s = sortBy (compare `on` snd) (M.toList c) 
+                              pp (msg, cnt) = (printf "%5d : %s" cnt $ show msg :: String)
+                              len = maximum $ 5: (map (length . pp) $ M.toList c)
+                          in unlines $ [replicate len '='] ++ map pp (M.toList c) 
+                                ++ [replicate len '=']
+
+emptyMessages :: Messages
+emptyMessages = Messages M.empty M.empty
+
+removeFromMessages :: Msg -> MVar Messages -> IO ()
+removeFromMessages msg state = modifyMVar_ state $ 
+    \(Messages c h) -> return $ Messages (M.delete msg c) (M.delete msg h)
 
 data Msg = Msg 
     { msgHostFrom :: HostAddress
@@ -28,6 +55,11 @@ data Msg = Msg
     , msgUser :: BS.ByteString
     }
 
+instance Eq Msg where
+    Msg h1 _ _ u1 == Msg h2 _ _ u2 = h1 == h2 && u1 == u2
+
+instance Ord Msg where
+    Msg h1 _ _ u1 `compare` Msg h2 _ _ u2 = h1 `compare` h2 <> u1 `compare` u2
 
 expandTo :: BS.ByteString -> Int -> BS.ByteString
 expandTo s n = let len = BS.length s in 
@@ -66,24 +98,43 @@ mkMsg = do
     t <- currentUnixTime64
     return $ Msg 0 "melchior" t "komarov"
 
+infoPrinter :: MVar Messages -> IO ()
+infoPrinter state = do
+    a <- readMVar state
+    print a
+    threadDelay printTimeout
+    infoPrinter state
+
 server :: IO ()
 server = do
+    state <- newMVar $ emptyMessages
+    forkIO $ infoPrinter state
     addr <- head <$> getAddrInfo
         (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
         Nothing (Just $ show port)
     sock <- socket (addrFamily addr) Datagram defaultProtocol
     bind sock (addrAddress addr)
-    loop sock
+    loop state sock
  where
-    loop sock = do
+    loop state sock = do
         (t, addr) <- recvFrom sock 100
-        (print :: Msg -> IO ()) $ decode $ BSL.fromStrict t
-        loop sock
+        let msg = decode $ BSL.fromStrict t
+        modifyMVar_ state $ \(Messages counts handlers) -> do
+            let newCounts = M.insertWith (+) msg 1 counts
+            case M.lookup msg handlers of
+                Just h -> killThread h
+                Nothing -> return ()
+            tid <- forkIO $ do
+                threadDelay killTimeout
+                removeFromMessages msg state
+            let newHandlers = M.insert msg tid handlers
+            return $ Messages newCounts newHandlers
+        loop state sock
         
 
 client :: IO ()
 client = do 
-    addr <- head <$> getAddrInfo Nothing (Just "255.255.255.255") (Just "44444")
+    addr <- head <$> getAddrInfo Nothing (Just "255.255.255.255") (Just $ show port)
     sock <- socket (addrFamily addr) Datagram defaultProtocol
     setSocketOption sock Broadcast 1
     loop $ \msg -> sendAllTo sock msg $ addrAddress addr
